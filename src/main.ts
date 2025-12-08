9434eb2a266840830b15f656b4b9ce0957383d76
bfereydo <bfereydouni023@gmail.com>
2025-12-08 03:55:48 -0800

    step 2 complete. Added a counter and some style changes. Button has an effect when mouse hovers over it or clicks

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
 import "./style.css";
 
-//creating bubble button with bubble emoji
+//creating main element that will contain the clicker button
+const app = document.createElement("main");
+app.className = "app";
+
+//creating header element with the game title
+const title = document.createElement("h1");
+title.textContent = "Bubble Clicker";
+
+//creates subtitle element
+const subtitle = document.createElement("p");
+subtitle.className = "subtitle";
+subtitle.textContent = "Tap the bubble to start popping!";
+
+//set up counter variable and bubble popped counter text
+let pops = 0;
+const counter = document.createElement("div");
+counter.className = "counter";
+counter.textContent = `Bubbles popped: ${pops}`;
+
+//creates the clickable bubble button
 const bubbleButton = document.createElement("button");
 bubbleButton.type = "button";
 bubbleButton.className = "bubble-button";
 bubbleButton.textContent = "🫧";
 bubbleButton.ariaLabel = "Pop the bubble";
 
-//when bubble is clicked, make the button 'click' down
+//incremenet counter when bubble button is clicked
 bubbleButton.addEventListener("click", () => {
-  bubbleButton.classList.add("bubble-pop");
-  globalThis.setTimeout(() => {
-    bubbleButton.classList.remove("bubble-pop");
-  }, 120);
+  pops += 1;
+  counter.textContent = `Bubbles popped: ${pops}`;
 });
 
-//renders the button to the screen
-document.body.append(bubbleButton);
+//append the button, title and subtitle texts to the main element created prior
+app.append(title, subtitle, counter, bubbleButton);
+
+//add the widget containing the button and titles to the page
+document.body.innerHTML = "";
+document.body.append(app);
